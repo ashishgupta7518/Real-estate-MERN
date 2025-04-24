1657ea3b25c75e196f15b3110c3eb9e52e676b1a
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-24 22:46:54 +0530

    create a middleware to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res ,next) => {
     const { username, email, password } = req.body;
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -20,10 +20,7 @@ export const signup = async (req, res) => {
         });
 
     } catch (error) {
-        return res.status(500).json(
-
-            error.message
-        );
+        next(error);
 
     }
 
