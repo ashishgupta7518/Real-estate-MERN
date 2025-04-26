1921d781b484eb8089771300e277b726636b2b72
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-26 12:17:13 +0530

    adding redux-persist-for local-storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import { composeWithDevTools } from '@redux-devtools/extension';
+import {persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig ={
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    },
+    reducer: persistedReducer,
 // other store enhancers if any can be added here
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 })
+
+export const persistor = persistStore(store)
