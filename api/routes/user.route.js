4eb0a5192365bdca2a7b170d186459e51de0ae7e
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-27 11:45:37 +0530

    create user upadte api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 
 const Router = express.Router();
 
 
 Router.get('/test', test);
+Router.post('/update/:id', verifyToken, updateUser);
+
 
 export default Router;
