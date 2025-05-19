8d578ac3eeac7667f7b7d0b1bb9435265c9c716a
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-05-19 20:50:46 +0530

    remove  path from backend

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,8 +25,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.use('/api/user', userRouter);
@@ -34,11 +34,6 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
