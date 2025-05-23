dd3c0449aa508b32bfbda9e0613c9b6dd0668bd0
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-05-23 22:01:32 +0530

    var update

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,12 +2,11 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  
-  if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+  let token = req.cookies?.access_token;
+
+  if (!token && req.headers.authorization?.startsWith('Bearer ')) {
     token = req.headers.authorization.split(' ')[1];
   }
-
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
