1a367869ca7ade6be552b0061fbb7b9799a49a91
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-05-25 21:10:04 +0530

    remove verify token

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { createListing, deleteListing ,updateListing ,getListing , getListings} from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+
 
 
 
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createListing);
-router.delete('/delete/:id', verifyToken, deleteListing);
-router.post('/update/:id', verifyToken, updateListing); // Assuming updateListing is similar to createListing
+router.post('/create',  createListing);
+router.delete('/delete/:id', deleteListing);
+router.post('/update/:id', updateListing); // Assuming updateListing is similar to createListing
 router.get('/get/:id' , getListing)
 router.get('/get', getListings); // Assuming getListing is similar to createListing
 
